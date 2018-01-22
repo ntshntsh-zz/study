0a3e171b890685e02dc7bce2ef3339ac43ca6b82
Natalya Naumova <ntsh@yandex-team.ru>
2018-01-23 01:13:32 +0300

    made project structure

---
+++ calc-client/src/main/java/ru/auto/ntsh/study/calc/Calculator.java
@@ -0,0 +1,14 @@
+package ru.auto.ntsh.study.calc;
+
+public class Calculator {
+
+    public float getSum (float firstValue, float secondValue)
+    {
+        return firstValue + secondValue;
+    }
+
+    public float getDiv (float value, float divider)
+    {
+        return value / divider;
+    }
+}
---
+++ calc-tests/src/test/java/ru/auto/ntsh/study/calc/ByJunitTests.java
@@ -0,0 +1,4 @@
+package ru.auto.ntsh.study.calc;
+
+public class ByJunitTests {
+}
