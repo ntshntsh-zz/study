edca4ea531a9cdf84d9235dbe391524b840c6f5f
Natalya Naumova <ntsh@yandex-team.ru>
2018-01-23 02:19:28 +0300

    add tests by jUnit

--- calc-tests/src/test/java/ru/ntsh/study/calc/ByJunitTests.java
+++ calc-tests/src/test/java/ru/ntsh/study/calc/ByJunitTests.java
@@ -1,4 +1,27 @@
 package ru.ntsh.study.calc;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ByJunitTests {
+
+    private static Calculator CALC = new Calculator();
+    private static float FIRST_VALUE = 5;
+    private static float SECOND_VALUE = 0.01F;
+
+
+    @Test
+    public void shouldSum() {
+        assertEquals(CALC.getSum(FIRST_VALUE, SECOND_VALUE),
+                FIRST_VALUE + SECOND_VALUE, 0);
+
+    }
+
+    @Test
+    public void shouldDiv() {
+        assertEquals(CALC.getDiv(FIRST_VALUE, SECOND_VALUE),
+                FIRST_VALUE / SECOND_VALUE, 0);
+    }
+
 }
