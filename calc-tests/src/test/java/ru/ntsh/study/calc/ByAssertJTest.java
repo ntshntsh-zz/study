b1d0270334909cf3f2ccc4000cd19933bfb1da8d
Natalya Naumova <ntsh@yandex-team.ru>
2018-01-23 16:08:48 +0300

    add first test by assertj

--- calc-tests/src/test/java/ru/ntsh/study/calc/ByAssertJTest.java
+++ calc-tests/src/test/java/ru/ntsh/study/calc/ByAssertJTest.java
@@ -1,4 +1,18 @@
 package ru.ntsh.study.calc;
 
+import org.junit.Test;
+
+import static  org.assertj.core.api.Assertions.assertThat;
+import static ru.ntsh.study.calc.testdata.CalcTestsData.*;
+
 public class ByAssertJTest {
+
+    private static Calculator CALC = new Calculator();
+
+    @Test
+    public void shouldSum()
+    {
+        assertThat(CALC.getSum(FIRST_VALUE,SECOND_VALUE))
+                .isEqualTo(FIRST_VALUE + SECOND_VALUE);
+    }
 }
