8f53fd87e92f29f90a99cdd2ff5ec2e4045d67f1
Natalya Naumova <ntsh@yandex-team.ru>
2018-01-23 15:39:09 +0300

    add second test by hamcrest assert

--- calc-tests/src/test/java/ru/ntsh/study/calc/ByHamcrestTest.java
+++ calc-tests/src/test/java/ru/ntsh/study/calc/ByHamcrestTest.java
@@ -11,8 +11,15 @@ public class ByHamcrestTest {
     private static Calculator CALC = new Calculator();
 
     @Test
+    public void shouldSum() {
+        assertThat("Результат сложения отличается от ожидаемого!",
+                CALC.getSum(FIRST_VALUE, SECOND_VALUE),
+                equalTo(FIRST_VALUE/SECOND_VALUE));
+    }
+    @Test
     public void shouldDiv() {
-        assertThat(CALC.getDiv(FIRST_VALUE, SECOND_VALUE),
+        assertThat("Результат деления отличается от ожидаемого!",
+                CALC.getDiv(FIRST_VALUE, SECOND_VALUE),
                 equalTo(FIRST_VALUE / SECOND_VALUE));
     }
 
