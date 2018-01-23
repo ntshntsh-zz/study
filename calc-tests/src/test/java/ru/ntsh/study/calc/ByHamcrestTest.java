37eec9e45ffb8738aba8bc142e491f56d45ab196
Natalya Naumova <ntsh@yandex-team.ru>
2018-01-23 15:34:30 +0300

    add first test by hamcrest assert

--- calc-tests/src/test/java/ru/ntsh/study/calc/ByHamcrestTest.java
+++ calc-tests/src/test/java/ru/ntsh/study/calc/ByHamcrestTest.java
@@ -1,6 +1,19 @@
 package ru.ntsh.study.calc;
 
-import
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static ru.ntsh.study.calc.testdata.CalcTestsData.*;
 
 public class ByHamcrestTest {
+
+    private static Calculator CALC = new Calculator();
+
+    @Test
+    public void shouldDiv() {
+        assertThat(CALC.getDiv(FIRST_VALUE, SECOND_VALUE),
+                equalTo(FIRST_VALUE / SECOND_VALUE));
+    }
+
 }
