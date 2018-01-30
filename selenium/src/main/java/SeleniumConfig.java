2b4359e3874a193f0040c09d1081128cf5f5c57b
Natalya Naumova <ntsh@yandex-team.ru>
2018-01-30 23:20:44 +0300

    add config

--- selenium/src/main/java/SeleniumConfig.java
+++ selenium/src/main/java/SeleniumConfig.java
@@ -3,4 +3,16 @@ import org.aeonbits.owner.Config;
 public interface SeleniumConfig extends Config {
     @Key("url.yandex")
     String urlYandex();
+
+    @Key("url.vk")
+    String urlVk();
+
+    @Key("vk.reciever")
+    String vkReciever();
+
+    @Key("vk.sender.login")
+    String vkSenderLogin();
+
+    @Key("vk.sender.password")
+    String vkSenderPassword();
 }
